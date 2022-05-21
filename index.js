f0d382d68e01e8860df1413a3a8cdb88deae571d
David Lucca <99742859+luccadavid@users.noreply.github.com>
2022-05-21 14:51:13 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    if (window.scrollY < inicio.offsetHeight) {
-        header1.style = " background-color: rgba(0, 0, 0, 0); ";
-    }
-    if (window.scrollY >= inicio.offsetHeight) {
-        header1.style = " background-color: rgb(4, 48, 0);  ";
-    }
-}
